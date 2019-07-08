cd75345d70d10a4d52167de276d87d26d2a98557
Tung Tran Thanh <tungtt22@fsoft.com.vn>
2019-07-08 21:53:00 +0700

    create switcher for receive command from slack

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# slack-lambda-functions
+# Slack with Lambda Function
+
+Build an Slack bot can use for manage Instances or Service on AWS
+
+## Contents
+
+- [Slack with Lambda Function](#Slack-with-Lambda-Function)
+  - [Contents](#Contents)
+  - [Building Slack Bot](#Building-Slack-Bot)
+  - [Configure AWS intergrate with Slack Bot](#Configure-AWS-intergrate-with-Slack-Bot)
+  - [Building Lambda Functions](#Building-Lambda-Functions)
+
+
+## Building Slack Bot
+
+## Configure AWS intergrate with Slack Bot
+
+## Building Lambda Functions
--- SlackAwsInstances/lambda_function.py
+++ SlackAwsInstances/lambda_function.py
@@ -1,27 +1,26 @@
 import json
 import SlackBotData
-
-body = "token=EtICe4Z3iBoPDrZJcnDUPLPn&team_id=TL239EXAR&team_domain=akaworkfpt&channel_id=CL1JHGQQ4&channel_name=akaworkio&user_id=UL239KT37&user_name=tungtt22&command=%2Fturnon&text=devops+sonar+bastion&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTL239EXAR%2F685159751284%2FEqJN9lE9jYVyjvhJzha4u6g4&trigger_id=687775014278.682111507365.7b35887deae71483412bc7a28172bb46"
+import Constants
 
 def lambda_handler(event, context):
     response = json.loads('{"message":"message"}')
-    # body = event.get('body')
-    data = json.loads("{\"" + body.replace("&","\",\"").replace("=","\":\"") + "\"}")
+    body = event.get('body').replace("&","\",\"").replace("=","\":\"")
+    data = json.loads("{\"" + body + "\"}")
     
     command = data["command"].strip("%2F")
     instances = data["text"].split("+")
-    print(instances)
-    sw = SlackBotData.Switcher()
-    sw.indirect(instances,command)
 
     if (SlackBotData.check_authorization(data)):
       response["message"] = "You are have authorization access to Lambda function!"
     else:
       response["message"] = "You are not have authorization access to Lambda function!"  
 
+    sw = SlackBotData.Switcher()
+    sw.action(command,"bastion")
+
     return {
         'statusCode': 200,
         'body': json.dumps(response)
     }
 
-print (lambda_handler("addd", "aaaa"))
+print (lambda_handler(json.loads(str(Constants.BODY)), "aaaa"))
