e0d16379e0cf5cde85c472cd3dcc254149294f99
Tung Thanh <thanhtungkma@gmail.com>
2019-07-21 22:24:56 +0700

    Update command turnof, turnoff, aws_status for RDS instance (#2)
    * [start] Update command turnof, turnoff, aws_status for RDS instance
    
    * [finished] Update command turnon, turnoff, aws_status for RDS instances

--- SlackAwsInstances/slackbot.py
+++ SlackAwsInstances/slackbot.py
@@ -59,38 +59,89 @@ class Command(object):
 
   def turnon(self, instance):
     ec2_instances = ec2.get_list_instances()
-    list_instance = ec2_instances
+    rds_instances = rds.get_list_instances()
+    list_instance = ec2_instances + rds_instances
     instances = get_instance_id(instance, list_instance)
-    value = ec2.start_all_instance(instances)
+    if (len(instances) == 0):
+      return "Instance not found!" 
+    elif (len(instances) == 1):
+      instance = instances[0]
+      if (instance["ServiceType"] == "ec2"):
+        value = ec2.start_instance(instance)
+      elif (instance["ServiceType"] == "rds"):
+        value = rds.start_instance(instance)
+    else:
+      for instance in instances:
+        if (instance["ServiceType"] == "ec2"):
+          value = ec2.start_all_instance(instances)
+        elif (instance["ServiceType"] == "rds"):
+          value = rds.start_all_instance(instances)
     return value
 
   def turnoff(self, instance):
     ec2_instances = ec2.get_list_instances()
-    list_instance = ec2_instances
+    rds_instances = rds.get_list_instances()
+    list_instance = ec2_instances + rds_instances
     instances = get_instance_id(instance, list_instance)
-    value = ec2.stop_all_instance(instances)
+    if (len(instances) == 0):
+      return "Instance not found!" 
+    elif (len(instances) == 1):
+      instance = instances[0]
+      if (instance["ServiceType"] == "ec2"):
+        value = ec2.stop_instance(instance)
+      elif (instance["ServiceType"] == "rds"):
+        value = rds.stop_instance(instance)
+    else:
+      for instance in instances:
+        if (instance["ServiceType"] == "ec2"):
+          value = ec2.stop_all_instance(instance)
+        elif (instance["ServiceType"] == "rds"):
+          value = rds.stop_all_instance(instance)
+
     return value
 
   def aws_status(self, instance):
     text  = ""
     ec2_instances = ec2.get_list_instances()
-    list_instance = ec2_instances
+    rds_instances = rds.get_list_instances()
+    list_instance = ec2_instances + rds_instances
     instances = get_instance_id(instance, list_instance)
     if (len(instances) > 0):
       for instance in instances:
-        text = (
-                  "The instance `{0}` has current status as below:\n" +
-                  "  - *Service Type:* `{1}`\n" +
-                  "  - *Status:* `{2}`\n" +
-                  "  - *Public IP:* `{3}`\n" +
-                  "  - *Local IP:* `{4}`\n" +
-                  "  - *Instance Type:* `{5}`\n"
-               ).format(
-                 instance["TagName"],
-                 instance["ServiceType"].upper(),
-                 instance["State"],
-                 instance["PublicIpAddress"],
-                 instance["PrivateIpAddresses"],
-                 instance["InstanceType"]
+        if (instance["ServiceType"] == "ec2"):
+          text = (
+                    "The instance `{0}` has current status as below:\n" +
+                    "  - *Service Type:* `{1}`\n" +
+                    "  - *Status:* `{2}`\n" +
+                    "  - *Public IP:* `{3}`\n" +
+                    "  - *Local IP:* `{4}`\n" +
+                    "  - *Instance Type:* `{5}`\n"
+                ).format(
+                  instance["TagName"],
+                  instance["ServiceType"].upper(),
+                  instance["State"],
+                  instance["PublicIpAddress"],
+                  instance["PrivateIpAddresses"],
+                  instance["InstanceType"]
                 )
+        elif (instance["ServiceType"] == "rds"):
+          text = (
+                    "The instance `{0}` has current status as below:\n" +
+                    "  - *Service Type:* `{1}`\n" +
+                    "  - *Status:* `{2}`\n" +
+                    "  - *Endpoint:* `{3}`\n" +
+                    "  - *Instance Type:* `{4}`\n" +
+                    "  - *Engine:* `{5}`\n" +
+                    "  - *Engine Version:* `{6}`\n"
+                ).format(
+                  instance["TagName"],
+                  instance["ServiceType"].upper(),
+                  instance["State"],
+                  instance["Endpoint"],
+                  instance["InstanceType"],
+                  instance["Engine"],
+                  instance["EngineVersion"]
+                )
+    else:
+      return "Instance not found!"
     return text
