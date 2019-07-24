831de61fba332296689e72d3aa444ca57ef7db15
Tung Tran Thanh <tungtt22@fsoft.com.vn>
2019-07-24 22:10:48 +0700

    [finished] Implement get instance tagname and fix code convention by using pylint

--- SlackAwsInstances/lambda_function.py
+++ SlackAwsInstances/lambda_function.py
@@ -1,44 +1,61 @@
+"""
+This file receive incoming event and first process event from Application
+"""
+
 import json
-import slackbot
-import constants
+import slackstash
+
 
 def process_event(event):
-  text = ""
-  att_text = ""
-  response = {
-    "response_type": "in_channel",
-    "text": None,
-    "attachments": []
-  }
-  body = event.get('body').replace("&", "\",\"").replace("=", "\":\"")
-  data = json.loads("{\"" + body + "\"}")
+    """
+    Process incoming event
+    """
+    text = ""
+    att_text = ""
+    response = {"response_type": "in_channel", "text": None, "attachments": []}
+    body = event.get('body').replace("&", "\",\"").replace("=", "\":\"")
+    data = json.loads("{\"" + body + "\"}")
 
-  command = data["command"].strip("%2F")
+    command = data["command"].strip("%2F")
 
-  data["text"] = " ".join(data["text"].replace("+", " ").split())
-  instances = data["text"].split(" ")
+    data["text"] = " ".join(data["text"].replace("+", " ").split())
+    instances = data["text"].split(" ")
 
-  if (slackbot.check_authorization(data)):
-    text = "User <@" + data[
-      "user_name"] + "> has run `" + command + " " + data["text"] + "` command"
-  else:
-    text = "User <@" + data[
-      "user_name"] + "> are not have authorization access to this function!"
-
-  cmd = slackbot.Command()
-  if "devops" == instances[0]:
-    if "all" in instances:
-      att_text = cmd.call(command, "all")
+    if slackstash.check_authorization(data):
+        text = "User <@{0}> has run `{1} {2}` command".format(
+            data["user_name"], command, data["text"])
     else:
-      att_text = cmd.call(command, instances[0] + "-" + instances[1])
-  else:
-    att_text = "Command `{0} {1}` wrong".format(command, data["text"].replace("+", " "))
+        text = "User <@{0}> are not have authorization access to this function!".format(
+            data["user_name"])
 
-  att_text = {"text": att_text}
-  response["text"] = text
-  response["attachments"].append(att_text)
-  return response
+    cmd = slackstash.Command()
+    if instances[0] == "turnon":
+        att_text = cmd.call(command + "_turnon", instances[1])
+    elif instances[0] == "turnoff":
+        att_text = cmd.call(command + "_turnoff", instances[1])
+    elif instances[0] == "status":
+        att_text = cmd.call(command + "_status", instances[1])
+    elif instances[0] == "tags":
+        att_text = cmd.call(command + "_tags", instances[1])
+    else:
+        att_text = "Command `{0} {1}` wrong".format(
+            command, data["text"].replace("+", " "))
+
+    att_text = {"text": att_text, "color": "#3AA3E3"}
+
+    response["text"] = text
+    response["attachments"].append(att_text)
+    return response
+
 
 def lambda_handler(event, context):
-  response = process_event(event)
-  return {"isBase64Encoded": True, 'statusCode': 200, 'body': json.dumps(response)}
+    """
+    Handler incoming request
+    """
+    print(context)
+    response = process_event(event)
+    return {
+        "isBase64Encoded": True,
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
